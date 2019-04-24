653575e041098915901136beb362695c683d7435
Anthony Yolach Lloveras <anthonylloveras.boxtypd@gmail.com>
2019-04-24 10:50:27 +0800

    commit sample

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,9 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 
 import { Hello } from "./components/Hello";
+import Login from './pages/Login';
 
 ReactDOM.render(
-    <Hello compiler="TypeScript" framework="React" />,
+    <Login />,
     document.getElementById("main-app")
 );
