6bbc6ac686c65e40a829f863ecf69a1bb7edcb3d
Anthony Yolach Lloveras <anthonylloveras.boxtypd@gmail.com>
2019-04-24 09:03:15 +0800

    initial commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import * as React from "react";
+import * as ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Hello } from "./components/Hello";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+    <Hello compiler="TypeScript" framework="React" />,
+    document.getElementById("main-app")
+);
